0329a4a5083ecf430dee4de8cd6e7fbdf4846e36
slam <hbiblia@gmail.com>
2018-02-11 22:24:55 -0400

    Update array_dynamic.h

--- array_dynamic.h
+++ array_dynamic.h
@@ -12,16 +12,19 @@ struct array{
 /* iniciamos un nuevo array */
 struct array *array_new(void)
 {
-    struct array *tmp = (struct array*)malloc(sizeof(struct array)+2);
+    struct array *tmp = (struct array*)malloc(sizeof(struct array)*2);
     tmp[0].length = 0;
     return tmp;
 }
 
 /* creamos un nuevo valor */
-void array_append(struct array *a, void *data)
+struct array *array_append(struct array *a, void *data)
 {
+    int l = (a[0].length == 0 ? 1 : a[0].length);
+    a = (struct array*)realloc(a, sizeof(struct array) * (l + 2));
     a[ a[0].length ].data = data;
     a[0].length++;
+    return a;
 }
 
 /* obtenemos un valor del array */
