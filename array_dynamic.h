7c32c7d59716e337ca1e9b2fb6f65f1046e65d75
Jhonson <hbiblia@gmail.com>
2023-12-09 01:05:31 -0400

    09-12-2023

--- array_dynamic.h
+++ array_dynamic.h
@@ -1,48 +1,113 @@
+/**
+ * @file array_dynamic.h
+ * @brief This file contains the MIT License for the array_dynamic library.
+ * @author Jhonson Ozuna - hbiblia@gmail.com
+ *
+ * The array_dynamic library provides functions for working with dynamically allocated arrays.
+ * It allows users to create, resize, access, and modify arrays of any data type.
+ *
+ * @license MIT License
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 
 #ifndef ARRAY_DYNAMIC_H
 #define ARRAY_DYNAMIC_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
-struct array{
-  void *data;
-  int length;
-};
+// estructura del array
+typedef struct PtrArray {
+    void** data; // Use double pointer to store array of pointers
+    int length;
+}PtrArray;
 
-/* iniciamos un nuevo array */
-struct array *array_new(void)
+/**
+ * @brief Crea un nuevo arreglo dinámico.
+ * 
+ * @return Puntero al nuevo arreglo dinámico.
+ */
+PtrArray* ad_array_new(void)
 {
-    struct array *tmp = (struct array*)malloc(sizeof(struct array)*2);
-    tmp[0].length = 0;
+    PtrArray* tmp = (PtrArray*)malloc(sizeof(PtrArray));
+    tmp->length = 0;
+    tmp->data = NULL;
     return tmp;
 }
 
-/* creamos un nuevo valor */
-struct array *array_append(struct array *a, void *data)
+/**
+ * @brief Libera la memoria asignada a un arreglo dinámico.
+ *
+ * @param array El puntero al arreglo dinámico.
+ */
+void ad_array_free(PtrArray* array)
 {
-    int l = (a[0].length == 0 ? 1 : a[0].length);
-    a = (struct array*)realloc(a, sizeof(struct array) * (l + 2));
-    a[ a[0].length ].data = data;
-    a[0].length++;
-    return a;
+    if (array == NULL) return;
+    free(array->data);
+    free(array);
 }
 
-/* obtenemos un valor del array */
-void *array_get(struct array *a, int index)
+/**
+ * Añade un elemento al final del arreglo dinámico.
+ *
+ * @param array El arreglo dinámico al que se desea añadir el elemento.
+ * @param data El dato que se desea añadir al arreglo.
+ */
+void ad_array_append(PtrArray* array, void* data)
 {
-    return a[index].data;
+    if (array == NULL) return;
+    array->data = (void**)realloc(array->data, sizeof(void*) * (array->length + 1));
+    array->data[array->length] = data;
+    array->length++;
 }
 
-/* borramos todo el array */
-void array_free(struct array *a)
+/**
+ * @brief Obtiene el elemento en la posición especificada del arreglo dinámico.
+ *
+ * @param array El arreglo dinámico.
+ * @param index El índice del elemento a obtener.
+ * @return Un puntero al elemento en la posición especificada, o NULL si el índice está fuera de rango.
+ */
+void* ad_array_get(PtrArray* array, int index)
 {
-    free(a);
+    if (array == NULL || index < 0 || index >= array->length) return NULL;
+    return array->data[index];
 }
 
-/* contamos la cantidad de datos que tienen el array */
-int array_length(struct array *a)
+/**
+ * @brief Remueve un elemento del arreglo dinámico en la posición especificada.
+ *
+ * @param array El arreglo dinámico del cual se removerá el elemento.
+ * @param index La posición del elemento a remover.
+ * @return Un puntero al elemento removido, o NULL si no se encontró el elemento o el arreglo está vacío.
+ */
+void* ad_array_remove(PtrArray* array, int index)
 {
-    return a[0].length;
+    if (array == NULL || index < 0 || index >= array->length) return NULL;
+    void* tmp = array->data[index];
+    for (int i = index; i < array->length - 1; i++)
+    {
+        array->data[i] = array->data[i + 1];
+    }
+    array->length--;
+    array->data = (void**)realloc(array->data, sizeof(void*) * array->length);
+    return tmp;
 }
 
 #endif
--- teste.c
+++ teste.c
@@ -5,19 +5,26 @@
 
 int main()
 {
-    struct array *tmp = array_new();
-    array_append(tmp, "Hola Mundo");
-    array_append(tmp, "Pokemon!!");
-    array_append(tmp, "Dragon Super");
-    
-    for(int i = 0; i<array_length(tmp); i++){
-        printf("Out: %s\n",array_get(tmp,i));
+    PtrArray *array = ad_array_new();
+
+    ad_array_append(array, "hola");
+    ad_array_append(array, "mundo");
+    ad_array_append(array, "cruel");
+    ad_array_append(array, "!");
+    ad_array_append(array, "aaaaah");
+
+    void* data = ad_array_remove(array, 4);
+
+    for (int i = 0; i < array->length; i++)
+    {
+        printf("%s\n", (char*)ad_array_get(array, i));
     }
 
-    array_free(tmp);
+    ad_array_free(array);
 
     return 0;
 }
+
 
 
 
